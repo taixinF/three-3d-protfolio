707f873d6e043eec1c4e3b46ad6da3263cff6de2
taixinF <yclzbb1349458861@163.com>
2023-03-07 07:33:29 +0800

    add nav

--- src/components/components/Navbar.jsx
+++ src/components/components/Navbar.jsx
@@ -2,12 +2,13 @@ import React, {useState} from 'react'
 import {styles} from "../../styles.js";
 import {Link} from "react-router-dom";
 import {logo} from "../../assets/index.js";
+import {navLinks} from "../../constants/index.js";
 
 const Navbar = () => {
     const [active, setActive] = useState("")
     return (
         <nav
-            className={`${styles.padding} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
+            className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
         >
             <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
                 <Link to="/" className="flex items-center gap-2" onClick={() => {
@@ -15,7 +16,23 @@ const Navbar = () => {
                     window.scrollTo(0, 0)
                 }}>
                     <img src={logo} alt="logo" className="w-9 h-9 object-contain"/>
+                    <p className="text-white text-[18px] font-bold cursor-pointer">TaiXin<span
+                        className="sm:block hidden">| JavaScript Mastery</span>
+                    </p>
                 </Link>
+                <ul className="list-none hidden sm:flex flex-row gap-10">
+                    {navLinks.map((link) => (
+                        <li
+                            key={link.id}
+                            className={`${
+                                active === link.title ? "text-white" : "text-secondary"
+                            } hover:text-white text-[18px] font-medium cursor-pointer`}
+                            onClick={() => setActive(link.title)}
+                        >
+                            <a href={`#${link.id}`}>{link.title}</a>
+                        </li>
+                    ))}
+                </ul>
             </div>
         </nav>
     )
