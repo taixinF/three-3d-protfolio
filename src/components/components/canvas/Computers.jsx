50eb4d233e4b7ed2b114e0c89b6faf5f579b4197
taixinF <yclzbb1349458861@163.com>
2023-03-09 08:13:12 +0800

    fix module

--- src/components/components/canvas/Computers.jsx
+++ src/components/components/canvas/Computers.jsx
@@ -1,12 +1,39 @@
-import React from 'react'
-import {useGLTF} from "@react-three/drei";
+import {Suspense, useEffect, useState} from 'react'
+import {OrbitControls, Preload, useGLTF} from "@react-three/drei";
+import {Canvas} from "@react-three/fiber";
 
 const Computers = () => {
-    const computer = useGLTF('./desktop_pc/scene.gltf')
+    const computer = useGLTF('./public/desktop_pc/scene.gltf')
+
     return (
         <mesh>
-            <hemisphereLight intensity={0.15} groundColor="block"/>
+            <hemisphereLight intensity={0.15} groundColor="black"/>
+            <primitive object={computer.scene}></primitive>
         </mesh>
+    )
+}
+
+
+const ComputersCanvas = () => {
+    return (
+        <Canvas
+            frameloop="demand"
+            shadows
+            camera={{position: [20, 3, 5], fov: 25}}
+            gl={{preserveDrawingBuffer: true}}
+        >
+            <Suspense
+                fallback={<CanvasLoader/>}
+            >
+                <OrbitControls
+                    enableZoom={false}
+                    maxPolarAngle={Math.PI / 2}
+                    minPolarAngle={Math.PI / 2}
+                />
+                <Computers></Computers>
+            </Suspense>
+            <Preload all/>
+        </Canvas>
     )
 }
 
