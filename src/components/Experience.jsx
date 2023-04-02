655f7e21787160d690802165fa6cd76907e1277e
taixinF <yclzbb1349458861@163.com>
2023-04-02 10:40:50 +0800

    fix fonts

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -61,10 +61,10 @@ const Experience = () => {
     <>
       <motion.div variants={textVariant()}>
         <p className={`${styles.sectionSubText} text-center`}>
-          What I have done so far
+          我到目前为止所做的一切
         </p>
         <h2 className={`${styles.sectionHeadText} text-center`}>
-          Work Experience.
+         工作经验
         </h2>
       </motion.div>
 
